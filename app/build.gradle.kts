14f934439d6febc1b9ddd8cd054f441c338bdaee
st10183958 <st10183958@myemeris.edu.za>
2026-09-01 11:16:26 +0200

    CI setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 android {
     namespace = "com.codehijackers.guldnet"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.codehijackers.guldnet"
         minSdk = 25
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 1
         versionName = "1.0"
 
